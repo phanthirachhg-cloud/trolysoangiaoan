45f7e029283b118460e49f901be22a8c27d22f91
phanthirachhg-cloud <phanthirach.hg@gmail.com>
2025-12-10 10:52:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Phàn Thị Rạch - Trường TH Thông Nguyên - ĐT: 0384 409 758</p>
 </div>
 """, unsafe_allow_html=True)
 
